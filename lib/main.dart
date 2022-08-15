beae9ba18ad9db7b1249e15026226ee4b1e6b7ba
hk-p114514 <hukada.gogotea@gmail.com>
2022-08-16 00:16:10 +0900

    success showing map by using mapbox url source

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:location/location.dart';
 
 void main() {
   runApp(const MyApp());
@@ -48,68 +50,61 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  int latitude = 0;
+  int longitude = 0;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: Container(
+        child: FlutterMap(
+          options: MapOptions(
+            zoom: 5.0,
+          ),
+          layers: [
+            // TileLayerOptions(
+            //   urlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+            //   subdomains: ['a', 'b', 'c'],
+            // ),
+            TileLayerOptions(
+                urlTemplate:
+                    'https://api.mapbox.com/styles/v1/hk-p/cl6mxxgcj001o15mxvjxr2gs9/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiaGstcCIsImEiOiJjbDYwNTV5ejUxNzlwM2RxaDY5Zm0xMXBoIn0.eig0zZUYx91cfE9reSSlIA',
+                additionalOptions: {
+                  'accessToken':
+                      'sk.eyJ1IjoiaGstcCIsImEiOiJjbDZwcndxaGwwazZ3M2N0NWlsN3M2cmtjIn0._hGqYOO9j9JcvRQn6gg-1w',
+                  'id': 'mapbox.mapbox-streets-v8',
+                }),
+
+            MarkerLayerOptions(
+              markers: [
+                Marker(
+                  width: 80.0,
+                  height: 80.0,
+                  point: LatLng(51.5, -0.09),
+                  builder: (ctx) => Container(
+                    child: const FlutterLogo(),
+                  ),
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+          ],
+          children: [
+            MaterialButton(
+              child: Text('Click Here'),
+              onPressed: () {
+                setState(() {
+                  // get location by using latlong package
+                });
+              },
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
