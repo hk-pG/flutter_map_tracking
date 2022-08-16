89c761734bafb82296e75b3f15ceedcbf5b514a5
hk-p114514 <hukada.gogotea@gmail.com>
2022-08-16 16:54:44 +0900

    fix android sdk version to build and require minimum

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
-import 'package:location/location.dart';
+import 'package:latlong2/latlong.dart';
 
 void main() {
   runApp(const MyApp());
