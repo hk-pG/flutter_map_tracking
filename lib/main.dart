cd1460b4ca6fb0747983cae419620ab289a47c0b
hk-p114514 <hukada.gogotea@gmail.com>
2022-08-17 11:27:09 +0900

    success tracking location by using 'location' and 'flutter_map'(mapbox)

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:location/location.dart';
 import 'package:latlong2/latlong.dart';
 
 void main() {
@@ -34,15 +37,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -50,21 +44,75 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int latitude = 0;
-  int longitude = 0;
+  double latitude = 0.0;
+  double longitude = 0.0;
+
+  void _getCurrentLocation() async {
+    Location location = Location();
+    bool isServiceEnabled = false;
+    PermissionStatus isPermissionGranted = PermissionStatus.denied;
+    isServiceEnabled = await location.serviceEnabled();
+    if (!isServiceEnabled) {
+      // request permission
+      isServiceEnabled = await location.requestService();
+
+      if (!isServiceEnabled) {
+        return;
+      }
+    }
+
+    isPermissionGranted = await location.hasPermission();
+    if (isPermissionGranted == PermissionStatus.denied) {
+      isPermissionGranted = await location.requestPermission();
+      if (isPermissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+
+    // 一一度だ位置情報を取得しに行く
+    // await location.getLocation().then((res) {
+    //   setState(() {
+    // latitude = res.latitude ?? 0;
+    // longitude = res.longitude ?? 0;
+    //   });
+    // });
+
+    // 位置が変化する度に情報を更新する
+    location.onLocationChanged.listen((res) {
+      setState(() {
+        latitude = res.latitude ?? 0;
+        longitude = res.longitude ?? 0;
+      });
+      print('latitude: $latitude, longitude: $longitude');
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    // _getCurrentLocation();
+    print("initState() called");
+  }
 
   @override
   Widget build(BuildContext context) {
+    _getCurrentLocation();
+    sleep(const Duration(seconds: 1));
+
     return Scaffold(
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Container(
+      body: SizedBox(
+        width: double.infinity,
         child: FlutterMap(
           options: MapOptions(
+            center: LatLng(latitude, longitude),
             zoom: 5.0,
+            maxZoom: 18.0,
+            minZoom: 1.0,
           ),
           layers: [
             // TileLayerOptions(
@@ -85,23 +133,28 @@ class _MyHomePageState extends State<MyHomePage> {
                 Marker(
                   width: 80.0,
                   height: 80.0,
-                  point: LatLng(51.5, -0.09),
+                  point: LatLng(latitude, longitude),
                   builder: (ctx) => Container(
-                    child: const FlutterLogo(),
+                    // Location marker
+                    child: Column(
+                      children: const [
+                        Text("YOU",
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                              backgroundColor: Colors.black,
+                            )),
+                        Icon(
+                          Icons.location_on,
+                          color: Colors.red,
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ],
             ),
-          ],
-          children: [
-            MaterialButton(
-              child: Text('Click Here'),
-              onPressed: () {
-                setState(() {
-                  // get location by using latlong package
-                });
-              },
-            )
           ],
         ),
       ),
